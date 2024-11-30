cfb80eb05790d2a88650545fcd2de35aad4bbed7
beboy01 <gbatofrancis@gmail.com>
2024-11-30 19:07:06 +0100

    Initialisation

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const Customer = () => {
+  return (
+    <div>page des clients</div>
+  )
+}
+export default Customer
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-wrap py-5'>Page des factures</div>
+  )
+}
+
+export default page
